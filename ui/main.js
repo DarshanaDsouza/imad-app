b4f114f349cf42979c0a7f08609202bff1a69aed
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-14 13:06:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,8 @@ console.log('Loaded!');
 
 
 //move the image
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = '100px';
 };
+*/
