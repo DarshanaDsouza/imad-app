50036a119aaf3e8540a9b21f904fd08c301095f5
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-14 15:05:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ app.get('/counter', function (req ,res) {
     
 });
 
-app.get('/ui/mymain.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'mymain.js'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/:articleName', function (req, res) {
