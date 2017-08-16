608e475baa8261f19366ecb14ff117ceeaf3e3a0
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-16 13:40:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,9 +15,9 @@ var name = nameInput.value;
 var submit = document.getElementById('submit-btn');
 submit.onclick=function(){
     
-    var names=[name1, name2, name3];
+    var names=['name1', 'name2', 'name3'];
     var list ='';
-    for (var i = 0 to names.length; i++){
+    for (var i = 0; i < names.length; i++){
         list += '<li>' +names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
