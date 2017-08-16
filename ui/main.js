1d1b199c6d7c0b86f2b08b51424aaa2d314a36cf
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-16 13:38:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,16 @@
   };
   
   
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit-btn');
+submit.onclick=function(){
+    
+    var names=[name1, name2, name3];
+    var list ='';
+    for (var i = 0 to names.length; i++){
+        list += '<li>' +names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    u1.innerHtml=list
+}
