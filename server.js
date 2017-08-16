27089622921a682832b34484d96662e9c649dc9a
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-16 18:34:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.get('/ui/main.js', function (req, res) {
 
 var names=[];
 app.get('/submit-name/:name', function (req, res) {
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     //JSON - Javascript object notification
     res.send(JSON.stringify(names));
