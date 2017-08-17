897f1074f0d8f854a248f3a56f14d7f14b868ae9
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-17 17:21:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,31 +146,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var submitcomm = document.getElementById('submitcomm');
-submitcomm.onclick = function(){
-   
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-        
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var comments = request.responseText;
-                comments = JSON.parse(comments);
-                var list = '';
-                for (var i = 0; i < comments.length; i++){
-                    list += '<li>' + comments[i] + '</li>';
-                }
-                var ul = document.getElementById('commlist');
-                ul.innerHTML=list;
-            }
-        }
-    };
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    request.open('GET','http://darshanadsouzaj.imad.hasura-app.io/submit-comm?comment=' + comment, true);
-    request.send(null);
-};
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
