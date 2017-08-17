f54a4000d0c155077066aa3531c3a3522932b88f
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-17 14:53:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,8 +18,8 @@ submit.onclick = function(){
     
     request.onreadystatechange = function(){
         
-        if (request.readystate === XMLHttpRequest.DONE){
-            if (request.status === 200){
+        if (request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
                 var names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
@@ -28,8 +28,8 @@ submit.onclick = function(){
                 }
                 var ul = document.getElementById('namelist');
                 ul.innerHTML=list;
-            };
-        };
+            }
+        }
     };
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
