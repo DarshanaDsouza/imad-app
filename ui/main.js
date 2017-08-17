fd245823c994197c9c83109fb543db17dbc0770d
DarshanaDsouza <30686927+DarshanaDsouza@users.noreply.github.com>
2017-08-17 16:47:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,3 +38,30 @@ submit.onclick = function(){
     request.open('GET','http://darshanadsouzaj.imad.hasura-app.io/submit-comm?name=' + name, true);
     request.send(null);
 };
+
+
+var submitcomm = document.getElementById('submit-comm');
+submitcomm.onclick = function(){
+   
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for (var i = 0; i < comments.length; i++){
+                    list += '<li>' + comments[i] + '</li>';
+                }
+                var ul = document.getElementById('commlist');
+                ul.innerHTML=list;
+            }
+        }
+    };
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    request.open('GET','http://darshanadsouzaj.imad.hasura-app.io/submit-comm?comment=' + comment, true);
+    request.send(null);
+};
